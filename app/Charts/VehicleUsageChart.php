d634c70e8974382b75db58e4b4c7ed58f2463bf5
akmlrnyn <akmalranyan@gmail.com>
2024-05-16 15:35:39 +0700

    fix the bar chart

--- app/Charts/VehicleUsageChart.php
+++ app/Charts/VehicleUsageChart.php
@@ -19,31 +19,25 @@ class VehicleUsageChart
     public function build(): \ArielMejiaDev\LarapexCharts\BarChart
     {
 
-        $permissions = Permission::selectRaw('MONTH(created_at) as monthly, COUNT(*) as count')->whereYear('created_at', date('Y'))->groupBy('monthly')->orderBy('monthly')->get();
+        $permissions = Permission::selectRaw('month as monthly, COUNT(*) as count')
+        ->whereYear('created_at', date('Y'))
+        ->groupBy('monthly')
+        ->orderBy('monthly')
+        ->get();
 
-        $labels = [];
-        $data = [];
+    $labels = [];
+    $data = [];
 
-        foreach ($permissions as $permission) {
-            $month = $permission->monthly;
-            $count = 0;
-
-
-            if ($month == $permission->monthly) {
-                $count = $permission->count;
-            }
-
-            array_push($labels, date('F', mktime(0,0,0,$month,1)));  
-            array_push($data, $count);
-        }
-
-
-
-
-     return $this->chart->barChart()
-            ->setTitle('Monthly Vehicle Usage')
-            ->setSubtitle('from all offices and vehicles | Month (X) Total Usage (Y)')
-            ->addData('Car Used', $data)
-            ->setXAxis($labels);
+    foreach ($permissions as $permission) {
+        $count = $permission->count;
+        array_push($labels, date('F', strtotime($permission->monthly.'-01'))); // Optional: Month names for labels
+        array_push($data, $count);
     }
+
+    return $this->chart->barChart()
+        ->setTitle('Monthly Vehicle Usage')
+        ->setSubtitle('from all offices and vehicles | Month (X) Total Usage (Y)')
+        ->addData('Car Used', $data)
+        ->setXAxis($labels);
+}
 }
